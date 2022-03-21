8ad594613a43a5b3cf2be02845eabfae77fba75b
zeunala <zeunala@naver.com>
2022-03-22 00:30:50 +0900

    Fail/1st/00:01:27

---
+++ Programmers/Lv2_59041.sql
@@ -0,0 +1,11 @@
+/*
+동명 동물 수 찾기 (mysql)
+
+- GROUP BY와 HAVING을 이용할 수 있다.
+* Fail/1st/00:01:27
+*/
+SELECT NAME, COUNT(*) AS COUNT
+FROM ANIMAL_INS
+GROUP BY NAME
+HAVING COUNT(*) >= 2
+ORDER BY NAME ASC
