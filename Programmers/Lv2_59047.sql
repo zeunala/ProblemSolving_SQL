63a5c0e2962c8a1cd7a73449a7107f0552c875ee
zeunala <zeunala@naver.com>
2022-03-29 00:19:27 +0900

    Fail/2nd/00:00:58

--- Programmers/Lv2_59047.sql
+++ Programmers/Lv2_59047.sql
@@ -3,7 +3,10 @@
 
 - LIKE를 이용하여 간단하게 작성할 수 있다.
 * Fail/1st/00:00:43
+- 결과를 이름 순으로 조회하도록 ORDER BY를 추가하였다.
+- Fail/2nd/00:00:58
 */
 SELECT ANIMAL_ID, NAME
 FROM ANIMAL_INS
 WHERE NAME LIKE "%el%"
+ORDER BY NAME ASC
